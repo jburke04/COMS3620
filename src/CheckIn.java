98ca496f07b3f338b45d91241db43048eb8e4302
Shubham <bhattacharya.shubham123@gmail.com>
2025-11-06 14:00:17 -0600

    added the Book in and check in classes and modified booking system

---
+++ src/CheckIn.java
@@ -0,0 +1,44 @@
+package src;
+
+public class CheckIn {
+    public static void start(Scanner scanner, BookingSystem system) {
+
+    }
+
+    private static void handleBookingNotFound(Scanner scanner, BookingSystem system){
+
+    }
+
+    private static void searchBookingsByGuestName(BookingSystem system, String name) {
+
+    }
+
+    private static void offerNewBooking(Scanner scanner, BookingSystem system) {
+
+    }
+
+    private static void handleEarlyCheckIn(Scanner scanner, Booking booking) {
+
+    }
+
+    private static void offerLuggageStorage(Scanner scanner) {
+
+    }
+
+    private static void handleRoomNotReady(Scanner scanner, BookingSystem system, Booking booking) {
+
+    }
+
+    private static boolean processPayment(Scanner scanner){
+
+    }
+
+    private static void handlePaymentFailure(Scanner scanner){
+
+    }
+
+    private static String formatDate(Calendar cal) {
+
+    }
+
+}
