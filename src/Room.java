f92d8b6211572ed95034568976314150bbbbd58c
tyler <tyler@laptop>
2025-11-05 20:25:31 -0600

    main loop

--- src/Room.java
+++ src/Room.java
@@ -1,5 +1,6 @@
 package src;
 public interface Room {
 	public int getCost();
-	public boolean book();
+	public boolean book(); //The way I see it is that all the business with "booking, changing, and checking out" can be represented at a higher level than the room itself.
+	public boolean unbook(); //Most likely we will have a helper class do all that for us. So, the room only knows when someone is or is not in it. Something something "information expert".
 }
