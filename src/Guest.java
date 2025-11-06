a46295395ad48b077c03f5b59636d0f0a1ed84da
Shubham <bhattacharya.shubham123@gmail.com>
2025-11-06 13:41:56 -0600

    completed guest.java for iteration1

--- src/Guest.java
+++ src/Guest.java
@@ -1,5 +1,30 @@
 package src;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Guest {
+	private static int nextId = 1;
+
+	int guestId;
 	String name;
+	String phoneNumber;
+	String email;
+	String idDocument;
+	List<Booking> bookingHistory;
+
+	public Guest(String name, String phoneNumber, String email, String idDocument) {
+		this.guestId = nextId++;
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+		this.idDocument = idDocument;
+		this.bookingHistory = new ArrayList<>();
+	}
+	// Geetrs and Setters
+	public int getGuestId() { return guestId; }
+	public String getName() { return name; }
+	public String getPhoneNumber() { return phoneNumber; }
+	public String getEmail() { return email; }
+	public List<Booking> getBookingHistory() { return bookingHistory; }
 }
