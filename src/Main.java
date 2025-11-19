5d367a2f8849798e15bcb66d1b5eac7b3e8db742
Shubham <bhattacharya.shubham123@gmail.com>
2025-11-19 16:20:58 -0600

    use case Hotel Menu System Completed

--- src/Main.java
+++ src/Main.java
@@ -12,41 +12,18 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         while (true) {
             System.out.println("\n=== HOTEL SYSTEM DEMO ===");
-            System.out.println("1) Book a Room");
-            System.out.println("2) Cancel a Booking");
-            System.out.println("3) Change Room");
-            System.out.println("4) In-Stay Maintenance");
-            System.out.println("5) Checkout & Payment");
-            System.out.println("6) Guest Lookup");
-            System.out.println("7) Update a Booking");
-            System.out.println("8) View all Bookings");
+            System.out.println("1) Guest Services");
+            System.out.println("2) Staff Operations");
             System.out.println("0) Exit");
             System.out.print("Choose: ");
             String choice = sc.nextLine().trim();
+
             switch (choice) {
                 case "1":
-                    BookRoom.start(sc, system);
+                    guestServiceMenu(sc, system);
                     break;
                 case "2":
-                    CancelService.start(sc, system);
-                    break;
-                case "3":
-                    ChangeRoom.start(sc, system);
-                    break;
-                case "4":
-                    InStayMaintenance.start(sc, system);
-                    break;
-                case "5":
-                    checkout.start(sc, system);
-                    break;
-                case "6":
-                    GetGuests.start(sc, system);
-                    break;
-                case "7":
-                    UpdateBookingService.start(sc, system);
-                    break;
-                case "8":
-                    ViewBookingsService.start(sc, system);
+                    staffOperationMenu(sc, system);
                     break;
                 case "0":
                     sc.close();
@@ -56,4 +33,77 @@ public class Main {
             }
         }
     }
+
+    /**
+     * Menu for guest-facing operations: booking, cancel, change, maintenance
+     * check in, checkout, payment, guest lookup, uodate booking
+     */
+    private static void guestServiceMenu(Scanner sc, HotelSystem system) {
+        System.out.println("\n=== GUEST SERVICES ===");
+        System.out.println("1) Book a Room");
+        System.out.println("2) Cancel a Booking");
+        System.out.println("3) Change Room");
+        System.out.println("4) In-Stay Maintenance");
+        System.out.println("5) Check In");
+        System.out.println("6) Checkout & Payment");
+        System.out.println("7) Guest Lookup");
+        System.out.println("8) Update a Booking");
+        System.out.println("0) Back to Main Menu");
+        System.out.println("Choose: ");
+        String choice = sc.nextLine().trim();
+        switch (choice) {
+            case "1":
+                BookRoom.start(sc, system);
+                break;
+            case "2":
+                CancelService.start(sc, system);
+                break;
+            case "3":
+                ChangeRoom.start(sc, system);
+                break;
+            case "4":
+                InStayMaintenance.start(sc, system);
+                break;
+            case "5":
+                checkout.start(sc, system);
+                break;
+            case "6":
+                GetGuests.start(sc, system);
+                break;
+            case "7":
+                UpdateBookingService.start(sc, system);
+                break;
+//            case "8":
+//                ViewBookingsService.start(sc, system);
+//                break;
+            case "0":
+                sc.close();
+                return;
+            default:
+                System.out.println("Invalid.");
+        }
+    }
+
+    /**
+     * Staff Menu - View all Booking
+     */
+    private static void staffOperationMenu(Scanner sc, HotelSystem system) {
+        System.out.println("\n=== STAFF OPERATIONS ===");
+        System.out.println("1) View all Bookings");
+        System.out.println("0) Back to Main Menu");
+        System.out.print("Choose: ");
+        String choice = sc.nextLine().trim();
+
+        switch (choice) {
+            case "1":
+                ViewBookingsService.start(sc, system);
+                break;
+            case "0":
+                return;
+            default:
+                System.out.println("Invalid.");
+        }
+    }
+
+
 }
